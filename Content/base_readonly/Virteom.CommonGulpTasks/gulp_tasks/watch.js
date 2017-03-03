c8a6c5dc86b2f1731ec277552353178604b87b7e
Phil Carbone <phil.carbone@virteom.com>
2017-03-03 12:19:15 -0500

    Removed solution files

--- Content/base_readonly/Virteom.CommonGulpTasks/gulp_tasks/watch.js
+++
@@ -1,7 +0,0 @@
-﻿var gulp = require('gulp');
-
-gulp.task('watch', function () {
-    gulp.watch("./Content/scss/**/*.scss", ["sass"]);
-    gulp.watch("./Content/**/*.ts", ["ts"]);
-    gulp.watch("./Content/**/*.html", ["html"]);
-});
--- Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-undefined
-[assembly: AssemblyVersion("0.0.10.5")]
-[assembly: AssemblyFileVersion("0.0.10.5")]
-[assembly: AssemblyInformationalVersion("0.0.10.5-prerelease")]

51d355f97006225922bf749f9456d6566ed47b9c
Phil Carbone <phil.carbone@virteom.com>
2017-03-03 08:51:04 -0500

    modified includes to keep autoload include seperate

--- includes.php
+++ includes.php
@@ -2,11 +2,6 @@
 
 if(!defined('INCLUDED_BUMPERLANE_PUBLIC_API_CLIENT')){
     define('INCLUDED_BUMPERLANE_PUBLIC_API_CLIENT', 1);
-    $autoloadFile = __DIR__ . '/../vendor/autoload.php';
-    if(file_exists($autoloadFile)){
-        include_once($autoloadFile);
-    }
-
     include_once(__DIR__ . '/Core/IApiClient.php');
     include_once(__DIR__ . '/Core/ClientModuleBase.php');
     include_once(__DIR__ . '/ApiClient.php');
